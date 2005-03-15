c3d19454db7e3a478b2957ae7e9626b9071bafc1
Jan Kneschke <jan@kneschke.de>
2005-03-15 09:59:29 +0000

    on win32 we need closesocket() instead of close()
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/post-upload-cleanup@140 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/file_descr.c
+++ src/file_descr.c
@@ -25,7 +25,11 @@ void file_descr_free(file_descr *fd) {
 void file_descr_reset(file_descr *fd) {
 	if (!fd) return;
 
+#ifdef __WIN32
+	if (fd->fd) closesocket(fd->fd);
+#else 
 	if (fd->fd) close(fd->fd);
+#endif
 
 	fd->fd = -1;
 	fd->fde_ndx = -1;
