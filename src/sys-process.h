3d27b79ba43a5bf84890ec7a459513ef35a7eb2a
Jan Kneschke <jan@kneschke.de>
2006-07-02 13:36:58 +0000

    r620@D2XNG01J:  Jan | 2006-06-26 03:29:16 +0200  provide emulations for unix-specific functions
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-merge-1.4.x@1129 152afb58-edef-0310-8abb-c4023f1b3aa9

---
+++ src/sys-process.h
@@ -0,0 +1,10 @@
+#ifndef _SYS_PROCESS_H_
+#define _SYS_PROCESS_H_
+
+#ifdef _WIN32
+#include <process.h>
+#else
+#include <unistd.h>
+#endif
+
+#endif
--- src/sys-socket.h
+++ src/sys-socket.h
@@ -1,15 +1,25 @@
 #ifndef WIN32_SOCKET_H
 #define WIN32_SOCKET_H
 
-#ifdef __WIN32
+#ifdef _WIN32
 
 #include <winsock2.h>
 
 #define ECONNRESET WSAECONNRESET
 #define EINPROGRESS WSAEINPROGRESS
 #define EALREADY WSAEALREADY
+#define ENOTCONN WSAENOTCONN
 #define ioctl ioctlsocket
 #define hstrerror(x) ""
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+#define ssize_t int
+
+int inet_aton(const char *cp, struct in_addr *inp);
+#define HAVE_INET_ADDR
+#undef HAVE_INET_ATON
+
 #else
 #include <sys/socket.h>
 #include <sys/ioctl.h>
